517aff85d772bae5c726a1ba0e5ad62217bb2c32
mamounjanfor <mamounjanfor@gmail.com>
2021-11-30 07:19:25 -0500

    chinese zodiac

---
+++ chinesezodiac.py
@@ -0,0 +1,14 @@
+"""
+a program that prompts the user to enter a year
+and displays the animal for the year.
+
+"""
+
+
+def chinese_year():
+    year = int(input("Enter the year"))
+    years = {"monkey": 0, "rooster": 1, "dog": 2, "pig": 4, "rat": 4, "ox": 5, "tiger": 6, "rabbit": 7, "dragon": 8, "snake": 9, "horse": 10, "sheep": 11}
+
+
+
+print(2000 % 12)
