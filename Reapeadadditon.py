a64a9e91287262f945a5673dcd1dae88e4c003be
mamounjanfor <mamounjanfor@gmail.com>
2021-11-30 08:18:53 -0500

    Repeat add init

---
+++ Reapeadadditon.py
@@ -0,0 +1,13 @@
+import random
+
+"""
+Recall the Simple Math Tool program from previous
+module that prompts the student to enter an answer
+for a question on addition of two single digit integers.
+"""
+
+
+def cal_two():
+    a = random.randint(0, 10)
+    b = random.randint(0, 10)
+    int(input("Enter you answer: "))
